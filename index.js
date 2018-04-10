1f1d1fe6c75f8a661b5f982b8666a52042ab754b
davidlatuno <dslatuno@gmail.com>
2018-04-09 19:55:59 -0700

    Added more user validation

--- index.js
+++ index.js
@@ -2,15 +2,23 @@
 var Word = require("./word.js");
 var inquirer = require("inquirer");
 
+// Used with user validation to only enter letters
+var letter = "abcdefghijklmnopqrstuvwxyz";
 
-var pie = new Word.Word("yellow");
+// List of words to choose from
+var state = ["alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut", "delaware", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new hampshire", "new jersey", "new mexico", "new york", "north carolina", "north dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode island", "south carolina", "south dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west virginia", "wisconsin", "wyoming"];
+
+var randomIndex = Math.floor(Math.random() * state.length);
+var randomWord = state[randomIndex];
+
+var computerWord = new Word.Word(randomWord);
 // Array for guessed letters
 var lettersGuessed = [];
 
 function hangman() {
     // Variable and function used to test if a letter guessed is correct
     var wordComplete = [];
-    pie.objArray.forEach(completeCheck);
+    computerWord.objArray.forEach(completeCheck);
 
     // Check if word has letters still to be guessed
     if (wordComplete.includes(false)) {
@@ -24,40 +32,45 @@ function hangman() {
             ])
             .then(function (input) {
 
-                // User Validation to not be able enter same letters more than once
-                if (lettersGuessed.includes(input.userinput)) {
-                    console.log("Already Guessed");
+                if (!letter.includes(input.userinput) || input.userinput.length > 1) {
+                    console.log("Not a Letter or too many inputs");
                     hangman();
                 } else {
 
-                    // Add guessed letter to array
-                    lettersGuessed.push(input.userinput);
-
-                    // Compare with word complete to see if a guess was correct
-                    var wordCheckArray = [];
-
-                    // Run guess through word constructor method to check if word includes use letter
-                    pie.userGuess(input.userinput);
-                    // Print the word to terminal
-                    pie.log();
-
-                    // Compare with word complete to see if a guess was correct
-                    pie.objArray.forEach(wordCheck);
-                    if (wordCheckArray.join('') === wordComplete.join('')) {
-                        console.log("Incorrect");
+                    // User Validation to not be able enter same letters more than once and only one letter at a time
+                    if (lettersGuessed.includes(input.userinput)) {
+                        console.log("Already Guessed ");
+                        hangman();
                     } else {
-                        console.log("Correct!");
-                    }
 
-                    // Call function again till word is complete
-                    hangman();
+                        // Add guessed letter to array
+                        lettersGuessed.push(input.userinput);
 
-                    // Function used with wordCheckArray forEach
-                    function wordCheck(key) {
-                        wordCheckArray.push(key.guessed);
+                        // Compare with word complete to see if a guess was correct
+                        var wordCheckArray = [];
+
+                        // Run guess through word constructor method to check if word includes use letter
+                        computerWord.userGuess(input.userinput);
+                        // Print the word to terminal
+                        computerWord.log();
+
+                        // Compare with word complete to see if a guess was correct
+                        computerWord.objArray.forEach(wordCheck);
+                        if (wordCheckArray.join('') === wordComplete.join('')) {
+                            console.log("Incorrect");
+                        } else {
+                            console.log("Correct!");
+                        }
+
+                        // Call function again till word is complete
+                        hangman();
+
+                        // Function used with wordCheckArray forEach
+                        function wordCheck(key) {
+                            wordCheckArray.push(key.guessed);
+                        }
                     }
                 }
-
             })
     } else {
         console.log("YOU WIN!");
